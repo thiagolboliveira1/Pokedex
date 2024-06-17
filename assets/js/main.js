d2a9ff80e2ac1f6006f88f421d9f23f8755fca4f
thiagolboliveira1 <thiagolboliveira@hotmail.com>
2024-06-17 14:56:32 -0300

    atualizando projeto

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+
+
+const offset = 0
+const limit = 10
+const url =`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonToHtml(pokemon) {
+    return `
+    <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+        
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>  
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>
+        </li> 
+    `
+}
+
+const pokemonList = document.getElementsById('pokemonList') pokemonList.appendChild('<li>Teste</li>')
+
+/*fetch(url)
+    .then( (response) =>  response.json())
+    .then( (jsonBody) => jsonBody.results)
+    .then( (pokemonList) => {
+        debugger
+        console.log(pokemonList)
+    })
+    .catch( (error) => console.error(error))
+    */
